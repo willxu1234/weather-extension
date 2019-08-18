2c4d5cfc9978ffd553118079b86958465a5a5c38
willxu1234 <wlxu@ucsd.edu>
2019-08-18 10:41:53 -0400

    Get dates from headers

--- extensionScript.js
+++ extensionScript.js
@@ -1,4 +1,13 @@
-let colheaders = document.querySelectorAll("div[role=\'presentation\'] div[role=\'columnheader\'] h2");
+// Pass a number, not a string.
+function dateKeyToDate(dateKey) {
+    let date = new Date('March 11, 1950');
+    date.setDate(date.getDate() + dateKey);
+    return date;
+}
+
+let colheaders = document.querySelectorAll("div[role='presentation'] div[role='columnheader'] h2");
 colheaders.forEach(el => {
-    el.style.backgroundColor = "red"
+    const dateKey = parseInt(el.querySelector("div[role='link']").getAttribute("data-datekey"), 10);
+    el.style.backgroundColor = "red";
+    el.textContent = dateKeyToDate(dateKey);
 })
